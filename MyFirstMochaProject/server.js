9982b739855678e3983ab5222ba53a472997379d
jesse <jwit11@avans.nl>
2017-05-03 15:59:36 +0200

    webPort_toegevoegd

--- MyFirstMochaProject/server.js
+++ MyFirstMochaProject/server.js
@@ -1,3 +1,17 @@
 /**
  * Created by jesse on 03/05/17.
  */
+var config = require('./config.json');
+
+var http = require('http');
+var express = require('express');
+
+var app = express();
+
+app.set('PORT', config.webPort);
+
+app.listen(8080, function() {
+    console.log('Server is running on port 8080');
+});
+
+module.exports = app;
